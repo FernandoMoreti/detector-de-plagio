aefc16c8957934157be3b3e9a4a70a885ca4eaf7
Fernando Moreti Bolela e Silva <153198754+FernandoMoreti@users.noreply.github.com>
2024-03-22 11:36:24 -0300

    retirando os comentarios
    retirando os comentarios para uma visualização mais clean

--- exercicios_do_curso/24.copiatryagain.py
+++ exercicios_do_curso/24.copiatryagain.py
@@ -1,7 +1,6 @@
 import re
 
 def le_assinatura():
-    '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
     print("Bem-vindo ao detector automático de COH-PIAH.")
     print("Informe a assinatura típica de um aluno infectado:")
 
@@ -15,7 +14,6 @@ def le_assinatura():
     return [wal, ttr, hlr, sal, sac, pal]
 
 def le_textos():
-    '''A funcao le todos os textos a serem comparados e devolve uma lista contendo cada texto como um elemento'''
     i = 1
     textos = []
     texto = input("Digite o texto " + str(i) +" (aperte enter para sair): ")
@@ -27,7 +25,6 @@ def le_textos():
     return textos
 
 def separa_sentencas(texto):
-    '''A funcao recebe um texto e devolve uma lista das sentencas dentro do texto'''
     sentencas = re.split(r'[.!?]+', texto)
     if sentencas[-1] == '':
         del sentencas[-1]
@@ -35,15 +32,12 @@ def separa_sentencas(texto):
     return sentencas
 
 def separa_frases(sentenca):
-    '''A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca'''
     return re.split(r'['',:;]+', sentenca)
 
 def separa_palavras(frase):
-    '''A funcao recebe uma frase e devolve uma lista das palavras dentro da frase'''
     return frase.split()
 
 def n_palavras_unicas(lista_palavras):
-    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras que aparecem uma unica vez'''
     freq = dict()
     unicas = 0
     for palavra in lista_palavras:
@@ -59,7 +53,6 @@ def n_palavras_unicas(lista_palavras):
     return unicas
 
 def n_palavras_diferentes(lista_palavras):
-    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras diferentes utilizadas'''
     freq = dict()
     for palavra in lista_palavras:
         p = palavra.lower()
@@ -71,7 +64,6 @@ def n_palavras_diferentes(lista_palavras):
     return len(freq)
 
 def compara_assinatura(as_a, as_b):
-    '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas.'''
     soma = 0
     for i in range(0,6):
         diferença = as_a[i] - as_b[i]
@@ -81,7 +73,6 @@ def compara_assinatura(as_a, as_b):
     return soma / 6
 
 def calcula_assinatura(texto):
-    '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
     a = tamanho_medio_palavra(texto)
     b = relaçao_type(texto)
     c = hapax_legomana(texto)
@@ -92,7 +83,6 @@ def calcula_assinatura(texto):
     return assinatura
     
 def avalia_textos(textos, ass_cp):
-    '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e uma assinatura ass_cp e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
     as_a = []
     maiorsimilaridade = 100000
     i= 0
